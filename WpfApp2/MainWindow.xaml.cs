22d614e831bcd196e3509cb527b5f00e16e0e9ac
svagatut123 <maratkhudayberdin5@gmail.com>
2026-01-28 11:21:35 +0300

    main win page1

---
+++ WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+using WpfApp2.pages;
+
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            BtnPage1_Click(null, null);
+        }
+        private void BtnPage1_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new Page1());
+
+        }
+        private void BtnPage2_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new Page2());
+
+        }
+        private void BtnPage3_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new Page3());
+
+        }
+    }
+}
