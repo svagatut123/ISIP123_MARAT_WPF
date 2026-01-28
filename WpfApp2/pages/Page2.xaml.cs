bf41abe7e1da713ff608fe4cf4107817fad4c9ab
svagatut123 <maratkhudayberdin5@gmail.com>
2026-01-28 11:37:33 +0300

    pg2

--- WpfApp2/pages/Page2.xaml.cs
+++ WpfApp2/pages/Page2.xaml.cs
@@ -1,28 +1,58 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using WpfApp2.pages;
 
 namespace WpfApp2.pages
 {
-    /// <summary>
-    /// Логика взаимодействия для Page2.xaml
-    /// </summary>
     public partial class Page2 : Page
     {
         public Page2()
         {
             InitializeComponent();
+            LoadCart();
+        }
+
+        private void LoadCart()
+        {
+            ListCart.ItemsSource = Cart.Tovary;
+            UpdateTotal();
+        }
+
+        private void UpdateTotal()
+        {
+            decimal total = Cart.Tovary.Sum(item => item.Summa);
+            TextTotal.Text = total.ToString("C");
+        }
+
+        private void RemoveItem_Click(object sender, RoutedEventArgs e)
+        {
+            var button = sender as Button;
+            var item = button.DataContext as CartItem;
+
+            if (item != null)
+            {
+                Cart.Tovary.Remove(item);
+                ListCart.Items.Refresh();
+                UpdateTotal();
+            }
+        }
+
+        private void ContinueShopping_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Page1());
+        }
+
+        private void CreateOrder_Click(object sender, RoutedEventArgs e)
+        {
+            if (Cart.Tovary.Count == 0)
+            {
+                MessageBox.Show("корзина пуста");
+                return;
+            }
+
+            NavigationService.Navigate(new Page3());
         }
     }
 }
