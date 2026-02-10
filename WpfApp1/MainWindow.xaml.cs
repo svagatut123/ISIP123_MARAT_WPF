35ee65f8062e5e42fcdac12a04a54b07bcd403fc
svagatut123 <maratkhudayberdin7@gmail.com>
2026-02-10 23:59:44 +0300

    mainwin

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,28 +1,73 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WpfApp1
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        public static user CurrentUser = null;
+
         public MainWindow()
         {
             InitializeComponent();
+            ShowMoviesPage();
+            UpdateAccountButton();
+        }
+
+        private void ShowMoviesPage()
+        {
+            var page = new MoviesPage();
+            MainFrame.Navigate(page);
+        }
+
+        private void UpdateAccountButton()
+        {
+            if (CurrentUser == null)
+            {
+                AccountButton.Content = "войти";
+            }
+            else
+            {
+                AccountButton.Content = "профиль (" + CurrentUser.first_name + ")";
+            }
+        }
+
+        private void SearchBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (MainFrame.Content is MoviesPage)
+            {
+                var page = (MoviesPage)MainFrame.Content;
+                page.ApplyFilter(SearchBox.Text);
+            }
+        }
+
+        private void SortComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (MainFrame.Content is MoviesPage && SortComboBox.SelectedItem != null)
+            {
+                var page = (MoviesPage)MainFrame.Content;
+                var item = (ComboBoxItem)SortComboBox.SelectedItem;
+                page.ApplySort(item.Content.ToString());
+            }
+        }
+
+        private void AccountButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (CurrentUser == null)
+            {
+                var page = new LoginPage();
+                MainFrame.Navigate(page);
+            }
+            else
+            {
+                var page = new ProfilePage();
+                MainFrame.Navigate(page);
+            }
+        }
+
+        public void RefreshAccountButton()
+        {
+            UpdateAccountButton();
         }
     }
 }
