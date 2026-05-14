ce6630b129a103543c48ad0b2ed42d8ea191036b
svagatut123 <maratkhudayberdin7@gmail.com>
2026-05-14 22:41:51 +0300

    5

--- WpfApp1/Core.cs
+++ WpfApp1/Core.cs
@@ -4,10 +4,8 @@ namespace WpfApp1
 {
     public static class Core
     {
-        // Статический контекст Entity Framework
         public static up_11Entities Context = new up_11Entities();
 
-        // Хранение текущего пользователя
         public static Users CurrentUser { get; set; }
     }
 }
